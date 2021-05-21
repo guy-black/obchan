887ba504cab761e2ecbcd99d45e6ca56f1ae2226
guy-black <guyb2602@gmail.com>
2021-05-21 14:57:19 -0500

    almost done with backend.  boy howdy i cant wait to see how many compiler errors i get when I'm done here

--- backend/src/Backend.hs
+++ backend/src/Backend.hs
@@ -7,6 +7,7 @@
 module Backend where
 
 import qualified Common.Route as R
+import Common.Api
 import Control.Monad.IO.Class (liftIO)
 import qualified Data.Aeson as A
 import Data.ByteString.Lazy (toStrict)
@@ -22,7 +23,7 @@ import Obelisk.Route (renderFrontendRoute, pattern (:/))
 import qualified Snap.Core as S
 
 maxPostSize :: Word64
-maxPostSize = 10000
+maxPostSize = 10000 --in bytes
 
 migration :: Query
 migration = "CREATE TABLE IF NOT EXISTS posts\
@@ -30,6 +31,40 @@ migration = "CREATE TABLE IF NOT EXISTS posts\
 
 backend :: Backend BackendRoute FrontendRoute
 backend = Backend
-  { _backend_run = \serve -> 
+--_backend_run :: ((R backendRoute -> Snap ()) -> IO ()) -> IO ()
+  { _backend_run = \serve -> do -- \serve :: (R backendRoute -> Snap ()) -> IO ()
+      withDb "db" $ \pool -> do -- \pool :: Pool Connection -> IO a
+        --pool is a Pool Connection, dbcon is the Connection
+        _ <- withResource pool $ \dbcon -> execute_ dbcon migration
+        serve $ \case -- \case::(R backendRoute -> Snap ())
+          R.BackendRoute_NewTh :/ () -> do
+            Just thread <- A.decode <$> S.readRequestBody maxPasteSize
+            --todo, a function to check that content is not null, and image is true
+            [[key]] <- liftIO $
+              withResource pool $ \dbcon ->
+                query dbcon "INSERT INTO posts (image,content,op) VALUES (?,?,?) RETURNING id" ((_postRequest_image thread) :: Bool, (_postRequest_content thread) :: (Maybe Text),Nothing :: (Maybe Int64))
+            S.modifyResponse $ S.setResponseStatus 200 "OK"
+            S.writeBS $ toStrict $ A.encode (key :: Int)
+          R.BackendRoute_NewCom :/ () -> do
+            --handle new comment
+          R.BackendRoute_ListPosts :/ () -> do
+            --return list of posts
+          R.BackendRoute_GetPost :/ key -> do
+            result <- liftIO $
+              withResource pool $ \dbcon ->
+                query dbcon "SELECT (image,content,op,datetime) FROM posts WHERE id = ?" [unId key]
+            case (result :: (Bool,(Maybe Text),(Maybe Int64),Text)) of
+              [(i,c,o,d)] -> S.writeBS $ toStrict $ A.encode $ PostResponse key i c o d   --if the post is real
+              _ -> S.modifyResponse $ S.setResponseStatus 404 "Not Found" --if its not found   
+          _ -> --else
+            S.redirect $
+              encodeUtf8 $
+                renderFrontendRoute R.checkedFullRouteEncoder $
+                  R.FrountedRoute_Main :/ ()
+      return () -- -> IO ()
   , _backend_routeEncoder = R.fullRouteEncoder
   }
+
+
+--when sending a query the values in the question mark are followed in a tuple, or singleton if only one
+--results from query come as a list of tuples, or list of singletons if only requesting one column
--- common/src/Common/Api.hs
+++ common/src/Common/Api.hs
@@ -15,14 +15,23 @@ import GHC.Generics (Generic)
 import Common.Schema (Post(..))
 import Database.Id.Class (Id(..))
 
-newtype PostRequest = PostRequest {
+data PostRequest = PostRequest {
                  _postRequest_content :: Maybe Text
                 ,_postRequest_image :: Bool
                 ,_postRequest_op :: Maybe (Id Post)
                 }
-  deriving stock Generic deriving newtype (ToJSON, FromJSON)
+  deriving stock Generic deriving data (ToJSON, FromJSON)
 
 
 commonStuff :: String
 commonStuff = "Here is a string defined in Common.Api"
 
+
+data PostResponse = PostResponse {
+                  _postResponse_id :: Int64
+                 ,_postResponse_image :: Bool
+                 ,_postResponse_content :: (Maybe Text)
+                 ,_postResponse_op :: (Maybe Int64)
+                 ,_postResponse_datetime :: Text}
+
+  deriving stock Generic deriving data (ToJSON, FromJSON) -- todo: figure out what this line means
